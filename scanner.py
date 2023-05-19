ffc91d4eff42613481cb1f3a12424af5321bb12c
T0tsuK4 <raul_FSM692@hotmail.com>
2023-05-19 18:07:52 -0300

    Scanner em Python

---
+++ scanner.py
@@ -0,0 +1,16 @@
+from xml.etree import ElementTree as etree
+from subprocess import Popen
+import argparse
+import os
+
+# Implemente a lógica para obter o IP com base no nome da máquina
+def get_ip(nome_maquina):
+    pass
+
+class Scanner:
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    scanner = Scanner()
+    print('Olá')
